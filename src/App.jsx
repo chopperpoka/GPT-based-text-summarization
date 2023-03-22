e122196cc7a0349ecea88408fc8af030aaa670b4
Bahaa Tantaoui <tantaouibahaabusiness@gmail.com>
2023-03-22 04:52:41 +0000

    fixed a typo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 import { Article, Brand, Cta, Feature, Navbar } from "./components";
+import {
+  Blog,
+  Features,
+  Footer,
+  Header,
+  Possibility,
+  WhatGPT3,
+} from "./containers";
 const App = () => {
   return (
     <div className="App">
