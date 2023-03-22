c8ed38556a817e47dd52bacaa940c2e722168868
Bahaa Tantaoui <tantaouibahaabusiness@gmail.com>
2023-03-22 17:03:13 +0000

    finished the css for now, started working on the Navbar component, and imported the assets we will need from Figma

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "./navbar.css";
+import { RiMenu3Line, RiCloseLin } from "react-icons/ri";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-logo">
+          <img src={logo} alt="logo"></img>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
