ca763001ccf209651ed0c90c3c6f6e2018b54c26
Bahaa Tantaoui <tantaouibahaabusiness@gmail.com>
2023-03-22 16:51:42 +0000

    added the CSS for the page, the gradient, the animation for every component and now working on the responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import "./App.css";
 const App = () => {
   return (
     <div className="App">
-      <div className="gradient__gb">
+      <div className="gradient__bg">
         <Navbar />
         <Header />
       </div>
