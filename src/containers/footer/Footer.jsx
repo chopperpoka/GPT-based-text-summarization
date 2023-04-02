81c6f77eebe1d282ebe01fc438c2829c25d50ba8
Bahaa Tantaoui <tantaouibahaabusiness@gmail.com>
2023-04-02 05:29:52 +0000

    more work on the UI

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -3,39 +3,42 @@ import logo from "../../assets/TextXtract-logo.png";
 import "./footer.css";
 
 const Footer = () => (
-  <div className="gpt3__footer section__padding">
-    <div className="gpt3__footer-links">
-      <div className="gpt3__footer-links_logo">
-        <img src={logo} alt="gpt3_logo" />
-        <p>
-          Crechterwoord K12 182 DK Alknjkcb, <br /> All Rights Reserved
-        </p>
+  <>
+    <hr />
+    <div className="gpt3__footer section__padding">
+      <div className="gpt3__footer-links">
+        <div className="gpt3__footer-links_logo">
+          <img src={logo} alt="gpt3_logo" />
+          <p>
+            Crechterwoord K12 182 DK Alknjkcb, <br /> All Rights Reserved
+          </p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Company</h4>
+          <p>Terms & Conditions </p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Get in touch</h4>
+          <p>Crechterwoord K12 182 DK Alknjkcb</p>
+          <p>085-132567</p>
+          <p>info@textxtract.net</p>
+        </div>
       </div>
-      <div className="gpt3__footer-links_div">
-        <h4>Links</h4>
-        <p>Overons</p>
-        <p>Social Media</p>
-        <p>Counters</p>
-        <p>Contact</p>
-      </div>
-      <div className="gpt3__footer-links_div">
-        <h4>Company</h4>
-        <p>Terms & Conditions </p>
-        <p>Privacy Policy</p>
-        <p>Contact</p>
-      </div>
-      <div className="gpt3__footer-links_div">
-        <h4>Get in touch</h4>
-        <p>Crechterwoord K12 182 DK Alknjkcb</p>
-        <p>085-132567</p>
-        <p>info@textxtract.net</p>
-      </div>
-    </div>
 
-    <div className="gpt3__footer-copyright">
-      <p>@2023 TextXtract. All rights reserved.</p>
+      <div className="gpt3__footer-copyright">
+        <p>@2023 TextXtract. All rights reserved.</p>
+      </div>
     </div>
-  </div>
+  </>
 );
 
 export default Footer;
