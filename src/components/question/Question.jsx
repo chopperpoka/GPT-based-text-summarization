d47cc28f0808f977c1e2e9d3aa345d1d16653fc2
Bahaa Tantaoui <tantaouibahaabusiness@gmail.com>
2023-04-04 06:54:48 +0000

    added a new component & container for the FAQ section

--- src/components/question/Question.jsx
+++ src/components/question/Question.jsx
@@ -1,4 +1,15 @@
-const Question = () => {
-  <div id="question"></div>;
+import "./question.css";
+import React from "react";
+
+const Question = (question, answer) => {
+  <div className="gpt3__FAQ-container__faqs">
+    <div className="gpt3__FAQ-container__-question">
+      <div />
+      <h1>{question}</h1>
+    </div>
+    <div className="gpt3__FAQ-container_feature-aanswer">
+      <p>{answer}</p>
+    </div>
+  </div>;
 };
 export default Question;
