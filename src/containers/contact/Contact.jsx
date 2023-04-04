992bc9f8872a3b62c0d4b3c61d48d569df541d1b
Bahaa Tantaoui <tantaouibahaabusiness@gmail.com>
2023-04-04 11:21:12 +0000

    added the fields for the contact form

--- src/containers/contact/Contact.jsx
+++ src/containers/contact/Contact.jsx
@@ -5,6 +5,16 @@ const Contact = () => {
     <div>
       <div className="gpt3__contact-text">
         <h1 className="heading">Contact us</h1>
+
+        <form className="contact-form">
+          <label>Full Name:</label>
+          <input type="text"></input>
+          <label>Work Email</label>
+          <input type="email"></input>
+          <label>Subject</label>
+          <input type="text"></input>
+          <input type="submit"></input>
+        </form>
       </div>
     </div>
   );
