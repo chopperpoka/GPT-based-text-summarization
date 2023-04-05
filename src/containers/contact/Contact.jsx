f0248b0eb8fcd28e4c2c41b227911e31ccf2c3fa
Bahaa Tantaoui <tantaouibahaabusiness@gmail.com>
2023-04-05 10:35:18 +0000

    fixed the last issue with the contact component

--- src/containers/contact/Contact.jsx
+++ src/containers/contact/Contact.jsx
@@ -3,27 +3,39 @@ import "./contact.css";
 const Contact = () => {
   return (
     <div id="contact">
-      <div className="gpt3__contact-text">
-        {/* fix the issue here on the gradient text with the contact header*/}
-        {/*TO DO */}
-        <div className="gradient__text">
-          <h1 className="contact-headline">Get In Touch:</h1>
-        </div>
-
-        <form className="contact-form">
-          <label>Full Name:</label>
-          <input className="contactInput" name="fullname" type="text"></input>
-          <label>Work Email</label>
-          <input className="contactInput" name="email" type="email"></input>
-          <label>Subject</label>
-          <input
-            className="contact-subjectInput"
-            name="subject"
-            type="text"
-          ></input>
-          <input className="submitInput" name="submit" type="submit"></input>
-        </form>
+      {/* fix the issue here on the gradient text with the contact header*/}
+      {/*TO DO */}
+      <div className="contact__headline">
+        <h1 className="gradient__text">Contact Us</h1>
       </div>
+      <form className="contact-form">
+        <label>Full Name:</label>
+        <input
+          className="contactInput"
+          name="fullname"
+          type="text"
+          required
+        ></input>
+        <label>Work Email</label>
+        <input
+          className="contactInput"
+          name="email"
+          type="email"
+          required
+        ></input>
+        <label>Subject</label>
+        <input
+          className="contact-subjectInput"
+          name="subject"
+          type="text"
+        ></input>
+        <input
+          className="submitInput"
+          name="submit"
+          type="submit"
+          required
+        ></input>
+      </form>
     </div>
   );
 };
