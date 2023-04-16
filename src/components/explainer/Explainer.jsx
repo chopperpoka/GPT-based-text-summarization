718a296dfc19aae3e880b97bb21260be5a7a2d06
Bahaa Tantaoui <tantaouibahaabusiness@gmail.com>
2023-04-16 00:00:35 +0000

    more styling on the demo page

--- src/components/explainer/Explainer.jsx
+++ src/components/explainer/Explainer.jsx
@@ -26,54 +26,59 @@ const Explainer = () => {
   };
 
   return (
-    <div className="demo__grid">
-      <div className="demo__grid-column-1">
-        <div className="demo__headline">
-          <h1 className="gradient__text">
-            {" "}
-            TextXtract: Generate Summaries of Long Texts with AI
-          </h1>
-        </div>
-        <p className="demo__description">
-          TextXtract is a powerful web application that uses advanced AI
-          algorithms to generate summaries of long articles, research papers,
-          and other texts. With TextXtract, you can input any text and receive a
-          concise summary that captures the most important information in just a
-          few sentences. Whether you're a student, researcher, or just a busy
-          professional, TextXtract is the perfect tool to save time and stay on
-          top of your reading.<br></br>
-          <br />{" "}
-          <strong>
-            Try it today and experience the power of GPT-based text
-            summarization!{" "}
-          </strong>
-        </p>
-      </div>
-      <div className="demo__grid-column-2">
-        <textarea
-          className="demo__text"
-          required
-          minLength={200}
-          placeholder="input the text you want to summarize here"
-          value={inputText}
-          onChange={handleInputChange}
-        ></textarea>
-        <div className="button_container">
-          <button
-            className="demo__submit"
-            onClick={handleSummarizeClick}
-            disabled={loading}
-          >
-            {loading ? "Loading..." : "Get Summary"}
-          </button>
-        </div>
-        {summary && (
-          <div className="demo__summary">
-            <h2 className="summary_headline">Summary:</h2>
-            <p>{summary}</p>
+    <div class="demo__page-layout">
+      <div className="demo__grid">
+        <div className="demo__grid-column-1">
+          <div className="demo__headline">
+            <h1 className="gradient__text">
+              {" "}
+              TextXtract: Generate Summaries of Long Texts with AI
+            </h1>
           </div>
-        )}
+          <p className="demo__description">
+            TextXtract is a powerful web application that uses advanced AI
+            algorithms to generate summaries of long articles, research papers,
+            and other texts. With TextXtract, you can input any text and receive
+            a concise summary that captures the most important information in
+            just a few sentences. Whether you're a student, researcher, or just
+            a busy professional, TextXtract is the perfect tool to save time and
+            stay on top of your reading.<br></br>
+            <br />{" "}
+            <strong>
+              Try it today and experience the power of GPT-based text
+              summarization!{" "}
+            </strong>
+          </p>
+        </div>
+        <div className="demo__grid-column-2">
+          <textarea
+            className="demo__text"
+            required
+            minLength={200}
+            placeholder="input the text that you want to summarize here"
+            value={inputText}
+            onChange={handleInputChange}
+          ></textarea>
+          <div className="button_container">
+            <button
+              className="demo__submit"
+              onClick={handleSummarizeClick}
+              disabled={loading}
+            >
+              {loading ? "Loading..." : "Summarize"}
+            </button>
+          </div>
+        </div>
       </div>
+      {/* THe summary section starts here*/}
+      {summary && (
+        <div className="demo__summary">
+          <div className="gradient__text">
+            <h2 className="summary_headline">Summary:</h2>
+          </div>
+          <p>{summary}</p>
+        </div>
+      )}
     </div>
   );
 };
