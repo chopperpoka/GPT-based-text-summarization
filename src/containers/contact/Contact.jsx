922c5df611712300b1a90666523324201621e02a
Bahaa Tantaoui <tantaouibahaabusiness@gmail.com>
2023-04-18 01:48:06 +0000

    added the useState hook and form sending logic to the contact component/container

--- src/containers/contact/Contact.jsx
+++ src/containers/contact/Contact.jsx
@@ -1,42 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import "./contact.css";
+
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    fullname: "",
+    email: "",
+    subject: "",
+  });
+
+  const handleFormChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+
+    // Opens email client with pre-populated fields
+    const recipientEmail = "youremail@example.com";
+    const subject = encodeURIComponent(`Form Submission - ${formData.subject}`);
+    const body = encodeURIComponent(
+      `Name: ${formData.fullname}\nEmail: ${formData.email}\nSubject: ${formData.subject}`
+    );
+    window.open(`mailto:${recipientEmail}?subject=${subject}&body=${body}`);
+  };
+
   return (
     <div id="contact">
-      {/* fix the issue here on the gradient text with the contact header*/}
-      {/*TO DO */}
       <div className="contact__headline">
         <h1 className="gradient__text">Contact Us</h1>
       </div>
-      <form className="contact-form">
+      <form className="contact-form" onSubmit={handleFormSubmit}>
         <label>Full Name:</label>
         <input
           className="contactInput"
           name="fullname"
           type="text"
+          value={formData.fullname}
+          onChange={handleFormChange}
           required
-        ></input>
+        />
         <label>Work Email</label>
         <input
           className="contactInput"
           name="email"
           type="email"
+          value={formData.email}
+          onChange={handleFormChange}
           required
-        ></input>
+        />
         <label>Subject</label>
         <input
           className="contact-subjectInput"
           name="subject"
           type="text"
-        ></input>
-        <input
-          className="submitInput"
-          name="submit"
-          type="submit"
-          required
-        ></input>
+          value={formData.subject}
+          onChange={handleFormChange}
+        />
+        <input className="submitInput" name="submit" type="submit" required />
       </form>
     </div>
   );
 };
+
 export default Contact;
