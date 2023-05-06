2d8881e15626a81270138da45b352a35a68bd3ac
Bahaa Tantaoui <tantaouibahaabusiness@gmail.com>
2023-05-06 23:46:43 +0100

    fixed a typo on the imports for the components folder

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,5 @@ export { default as Brand } from "./brand/Brand";
 export { default as CTA } from "./cta/CTA";
 export { default as Feature } from "./feature/Feature";
 export { default as Navbar } from "./navbar/Navbar";
-export { default as Question } from "./question/question";
+export { default as Question } from "./question/Question";
 export { default as Explainer } from "./explainer/Explainer";
