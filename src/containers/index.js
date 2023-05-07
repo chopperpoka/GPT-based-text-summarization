62ed29d3f012af835c972db85c67907bf2f1dc8b
Bahaa Tantaoui <tantaouibahaabusiness@gmail.com>
2023-05-07 03:45:34 +0100

    changed imports

--- src/containers/index.js
+++ src/containers/index.js
@@ -2,6 +2,6 @@ export { default as Features } from "./features/Features";
 export { default as Footer } from "./footer/Footer";
 export { default as Header } from "./header/Header";
 export { default as Possibility } from "./possibility/Possibility";
-export { default as About } from "./about/About";
 export { default as Faq } from "./faqs/Faq";
+export { default as About } from "./about/About";
 export { default as Contact } from "./contact/Contact";
