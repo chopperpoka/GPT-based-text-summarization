4725c77d605eb92a2d2aa3b5deaa134b7ffcedf3
Bahaa Tantaoui <tantaouibahaabusiness@gmail.com>
2023-05-10 04:12:37 +0100

    added vercel.json and updated start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -1,24 +1,6 @@
 #!/bin/bash
 
-# Exit early on errors
-set -eu
+export FLASK_APP=app.py
+export FLASK_ENV=production
 
-# Python buffers stdout. Without this, you won't see what you "print" in the Activity Logs
-export PYTHONUNBUFFERED=true
-
-# Install Python 3 virtual env
-VIRTUALENV=.data/venv
-
-if [ ! -d $VIRTUALENV ]; then
-  python3 -m venv $VIRTUALENV
-fi
-
-if [ ! -f $VIRTUALENV/bin/pip ]; then
-  curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/python
-fi
-
-# Install the requirements
-$VIRTUALENV/bin/pip install -r requirements.txt
-
-# Run a glorious Python 3 server
-$VIRTUALENV/bin/python3 app.py
+./.data/venv/bin/python -m flask run --host=0.0.0.0 --port=$PORT
